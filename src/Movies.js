ac03d03cd478d80dc5694333b5d0fde104b342a0
Priti Tripathi <tmcproject@protonmail.com>
2023-05-28 16:05:49 +0530

    Movies files

---
+++ src/Movies.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+import { NavLink } from 'react-router-dom';
+
+const Movies = () => {
+    const { movie } = useGlobalContext();
+
+    return (
+
+        <section className='movie-page'>
+            <div className='container grid grid-4-col'>
+                {movie.map((curMovie) => {
+                    const { imdbID, Title, Poster, Year, Type } = curMovie;
+                    const movieName = Title.substring(0, 15);
+                    return <NavLink to={`movie/${imdbID}`} key={imdbID}>
+                        <div className='card '>
+                            <div className='card-info'>
+                                {/* <h2>{Title}</h2> */}
+                                <h2>{movieName.length >= 15 ? `${movieName}...` : movieName}</h2>
+                                <img src={Poster} alt={imdbID} />
+                                <p>{Year}</p>
+                                <p>{Type}</p>
+                            </div>
+                        </div>
+                    </NavLink>;
+                })}
+            </div>
+        </section>
+
+    )
+}
+
+export default Movies
---
+++ src/context.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState } from "react"
+
+// Context (warehouse)
+// Provider ()
+// Consumer useContext()
+
+
+const AppContext = React.createContext();
+
+const API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY}`;
+//provider fun
+const AppProvider = ({ children }) => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const [isError, setIsError] = useState({show:"false", msg: ""})
+    const [query, setQuery] = useState("titanic")
+
+    const getMovies = async(url) => {
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+            if(data.Response === "True") {
+                setIsLoading(false);
+                setIsError({
+                    show: false,
+                    msg: "",
+                })
+                setMovie(data.Search);
+            }else {
+                setIsError({
+                    show: true,
+                    msg: data.Error,
+                })
+            }
+        }catch (error){
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+
+        let timerOut = setTimeout(() => {
+            getMovies(`${API_URL}&s=${query}`);
+        },800)
+
+        return () => clearTimeout(timerOut);
+
+    }, [query]);
+
+    return <AppContext.Provider value={{isLoading, movie, isError, query, setQuery}}>
+        {children}
+    </AppContext.Provider>
+}
+
+
+//global custom hooks
+
+const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+
+export { AppContext, AppProvider, useGlobalContext }
